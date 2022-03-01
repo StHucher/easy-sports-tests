02ebf01540a7512029cd9a2b7fe4037cb9d4bfa2
StHucher <stephane.hucher40@gmail.com>
2022-03-01 11:51:54 +0100

    Function newUser

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +18,28 @@ class HomeController extends AbstractController
     {
         return $this->render('home/homepage.html.twig');
     }
+
+    /**
+     * @Route("/subscribe", name="homesubscription", methods={"GET", "POST"})
+     */
+    /* public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('home/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    } */
 
     /**
      * @Route("/contact", name="contactpage")
