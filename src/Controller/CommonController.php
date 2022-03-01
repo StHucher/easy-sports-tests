985fab904a097381950653c5836f2435cdb455d0
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2022-03-01 13:34:05 +0100

    method + route

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -2,19 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\TagTest;
+use App\Entity\Test;
+use App\Entity\User;
+use App\Repository\TagTestRepository;
+use App\Repository\TestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
 
 class CommonController extends AbstractController
 {
     /**
-     * @Route("/common", name="app_common")
+     * @Route("/home/{slug}", name="user_home")
+     * 
+     * @return Response
      */
-    public function index(): Response
+    public function home(User $user): Response
     {
-        return $this->render('common/all_tests.html.twig', [
-            'controller_name' => 'CommonController',
-        ]);
+        return $this->render('common/home.html.twig',['user'=> $user]);
+    }
+
+    /**
+     * @Route("/history/{slug}", name="history")
+     *
+     * @return Response
+     */
+    public function history(User $user) : Response
+    {   
+
+        return $this->render('common/test-history.html.twig',['user'=> $user]);
+    }
+
+    /**
+     * @Route("/tests", name="tests")
+     *
+     * @return Response
+     */
+    public function tests() : Response
+    {
+        return $this->render('common/all_tests.html.twig');
+    }
+
+    /**
+     * @Route("/tests/physique", name="test_physique")
+     *
+     * @return Response
+     */
+    public function testPhysique() : Response
+    {
+        
+        return $this->render('common/technical_tests.html.twig');
+    }
+
+    /**
+     * @Route("/tests/technique", name="test_technique")
+     *
+     * @return Response
+     */
+    public function testTechnique() : Response
+    {   
+        
+        return $this->render('common/technical_tests.html.twig');
     }
 }
