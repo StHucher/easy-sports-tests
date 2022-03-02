4d97bac8c6cad5ff1577b6e4b187cc229cb09c1c
StHucher <stephane.hucher40@gmail.com>
2022-03-02 12:57:05 +0100

    Repeat password field

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType as TypeDateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -34,13 +35,15 @@ class UserType extends AbstractType
                 'multiple' => true,
                 'expanded' => true
             ])
-            ->add('password', PasswordType::class, [
-                
-                    // instead of being set onto the object directly,
-                    // this is read and encoded in the controller
-                    /* 'mapped' => false, */
-                    'attr' => ['autocomplete' => 'new-password'],
-                    'constraints' => [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe'],
+
+                'constraints' => [
                         new NotBlank([
                             'message' => 'Un mot de passe est nécessaire',
                         ]),
