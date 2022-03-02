4cffc5ef3202e51efc416d2a808b7583be46457d
OlivierLartillot <olivier.lartillot@gmail.com>
2022-03-02 22:08:47 +0100

    add player to team with bug in redirection because of the id in route

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -3,12 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Activity;
+use App\Entity\Team;
+use App\Entity\User;
 use App\Form\ActivityType;
 use App\Repository\ActivityRepository;
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("coach/activity")
@@ -26,18 +30,25 @@ class ActivityController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="app_activity_new", methods={"GET", "POST"})
+     * @Route("/new/{id}", name="app_activity_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, ActivityRepository $activityRepository): Response
+    public function new(Request $request, ActivityRepository $activityRepository, TeamRepository $team, $id): Response
     {
         $activity = new Activity();
         $form = $this->createForm(ActivityType::class, $activity);
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $myTeam = $team->find($id);
+            //dd($myTeam);
+            $activity->setRole('0');
+            $activity->setTeam($myTeam);
+ 
             $activityRepository->add($activity);
     
-            return $this->redirectToRoute('app_activity_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('coach_teams', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('activity/new.html.twig', [
