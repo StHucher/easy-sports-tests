33f384b7007806c5ae9ebdca7ef8ec3189c5d550
OlivierLartillot <olivier.lartillot@gmail.com>
2022-03-06 22:42:26 +0100

    admin Test crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Team;
+use App\Entity\Test;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -15,18 +17,21 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+
+
+        return $this->render(('admin/dashboard.html.twig'));
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Projet 20 Easy Sports Tests');
+            ->setTitle('Admin Easy Sports Tests');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Tests', 'fas fa-list', Test::class);
     }
 }
