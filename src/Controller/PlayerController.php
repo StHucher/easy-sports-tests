292bb3d25d9618045a4000a51cb587118e318a5c
OlivierLartillot <olivier.lartillot@gmail.com>
2022-03-08 21:55:24 +0100

    charts official and treaining added

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ResultRepository;
+use App\Repository\TestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,42 +27,83 @@ class PlayerController extends AbstractController
     /**
      * @Route("/chart/{id}", name="app_chart")
      */
-    public function chart(ChartBuilderInterface $chartBuilder, Security $security, $id): Response
+    public function chart(ChartBuilderInterface $chartBuilder, Security $security, $id, ResultRepository $resultRepository, TestRepository $testRepository): Response
     {
-
+        //id du test
         $testId = $id;
+        // récupère le test pour le name par exemple, description, photo etc
+        $test = $testRepository->find($id);
+        //user courant
         $user = $security->getUser();
-
-        $myResults = $user->getResults();
         
+        $myResults = $resultRepository->findBy([
+            'test'=>$testId,
+            'user' => $user,
+        ]);
 
-        //creer le tableau de resultDatas pour ce test
-        $resultData = [];
+        $resultOfficialsData = [];
+        $resultOfficialsDate = [];
+        $resultTrainingsData = [];
+        $resultTrainingsDate = [];
+
         foreach ($myResults as $result) {
-            if ($result->getTest()->getId() == $testId) {
-                $resultData [] = $result->getResult();
+       
+            if ($result->getStatus() == 1) {
+                $resultOfficialsDate [] = $result->getDoneAt()->format('m-Y'); 
+                $resultOfficialsData [] = $result->getResult();
             }
-            
+            else {
+                $resultTrainingsDate [] = $result->getDoneAt()->format('m-Y'); 
+                $resultTrainingsData [] = $result->getResult();
+            }
         }
 
         $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
-        $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        $chart->setData(
+            [
+                'labels' => $resultOfficialsDate,
+                'datasets' => [
+                    [
+                        'label' => 'officiel',
+                        'backgroundColor' => 'rgb(255, 99, 132)',
+                        'borderColor' => 'rgb(255, 99, 132)',
+                        'data' => $resultOfficialsData,
+                    ], 
+                ] 
+            
+            ],   
+    
+    );
+
+
+        $chart2 = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart2->setData([
+            'labels' => $resultTrainingsDate,
             'datasets' => [
                 [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => $resultData,
+                    'label' => 'Entrainement',
+                    'backgroundColor' => 'rgb(0, 99, 132)',
+                    'borderColor' => 'rgb(0, 99, 132)',
+                    'data' => $resultTrainingsData,
                 ],
+                  /*               [
+                    'label' => 'entrainement',
+                    'backgroundColor' => 'rgb(0, 99, 132)',
+                    'borderColor' => 'rgb(0, 99, 132)',
+                    'data' => ['52','45'],
+                ], */
             ],
         ]);
 
         $chart->setOptions([/* ... */]);
 
         return $this->render('common/chart.html.twig', [
+            'test' => $test,
             'chart' => $chart,
+            'chart2' => $chart2,
             'user' => $user, 
+            'myresults' => $myResults 
+  
         ]);
     }
 }
